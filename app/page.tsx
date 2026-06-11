345e57d5cdc9d10b2ab1cee4ff26069cff8a564c
Niklas-Nordin <niklas.nordin@chasacademy.se>
2026-06-11 10:40:03 +0200

    Changing max width to login form

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
 
   return (
     <div className="flex flex-col gap-10">
-      <div className="min-h-[60vh] w-full p-10 flex flex-col items-center gap-6 bg-gradient-to-br from-space-dark via-space-teal to-space-dark lg:flex-row lg:gap-25 lg:pr-30">
+      <div className="min-h-[60vh] w-full p-10 flex flex-col items-center gap-6 bg-gradient-to-br from-space-dark via-space-teal to-space-dark lg:flex-row lg:gap-30 lg:pr-30">
         <div className="flex flex-col items-center text-center w-full md:mx-auto md:items-start text-space-light gap-4">
           <h1 className="text-3xl font-bold text-space-light lg:text-left lg:text-5xl">Lär dig investering utan risk med <span className="text-white">100 000 SEK</span> i demokapital</h1>
           <h2 className="text-space-light mb-4 lg:text-left lg:text-2xl">Upplev spänningen i investeringsmarknaden utan att riskera riktiga pengar. Vår simulator ger dig realtidsdata och verktygen du behöver för att bli en expert.</h2>
         </div>
-        <div className="w-full max-w-sm md:max-w-md mx-auto bg-white p-4 border border-gray-300 rounded shadow">
+        <div className="w-full max-w-sm mx-auto bg-white p-4 border border-gray-300 rounded shadow">
           <div className="text-xl font-bold mb-4">
             <button className={`w-[50%] p-2 cursor-pointer ${isLogin ? 'border-b-2 border-space-dark text-space-dark' : 'border-b-2 border-gray-400 text-gray-400'}`} onClick={() => setIsLogin(true)}>
               Logga in
